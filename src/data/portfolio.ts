e2862796a5efbdd56a198a127172b085d419beeb
Kevin Barcenas <kevin14767@icloud.com>
2025-05-23 00:12:38 -0400

    added aniver font to portfolio, color changes, and beginning of refactoring

---
+++ src/data/portfolio.ts
@@ -0,0 +1,28 @@
+
+
+export const tocItems = [
+  { id: 'about-me', label: 'About Me', depth: 1 },
+  { id: 'education', label: 'Education', depth: 2 },
+  { id: 'skills', label: 'Skills & Technologies', depth: 2 },
+  { id: 'projects', label: 'Projects', depth: 1 },
+  { id: 'trucking-logistics', label: 'Trucking Logistics', depth: 2 },
+  { id: 'empire-football', label: 'Empire Football League', depth: 2 },
+  { id: 'experience', label: 'Experience', depth: 1 },
+  { id: 'contact', label: 'Contact Information', depth: 1 },
+  { id: 'resume', label: 'Resume', depth: 1 },
+];
+
+export const skills = [
+  { 
+    category: 'Technical Skills', 
+    skills: 'Python, TypeScript, Javascript, Java, C, Objective-C, Swift, HTML/CSS, NLTK, CLTK, Pandas, NumPy' 
+  },
+  { 
+    category: 'Frameworks & Tools', 
+    skills: 'Next.js, React Native, Expo, MongoDB, MySQL, PostgresSQL, Firebase, Supabase, NextAuth, Google Cloud Platform, Docker, AWS, Xcode' 
+  },
+  { 
+    category: 'Languages', 
+    skills: 'Spanish (Native), English (Native)' 
+  }
+];
